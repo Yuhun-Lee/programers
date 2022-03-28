985526b4d8b6b4804014c9dba2f803ee687bf098
juhoon <wngns8334@gmail.com>
2022-03-29 02:58:31 +0900

    다트 게임

--- coding test swift/main.swift
+++ coding test swift/main.swift
@@ -1,68 +1,35 @@
 import Foundation
 
-func solution(_ info:[String], _ query:[String]) -> [Int] {
-    var db: [String : [Int]] = [ : ]
-    var result: [Int] = []
-
-    // db 생성
-    for i in info {
-        let arr = i.components(separatedBy: " ")
-        let languages = [arr[0], "-"]
-        let jobs = [arr[1], "-"]
-        let careers = [arr[2], "-"]
-        let soulFoods = [arr[3], "-"]
-        let score = Int(arr[4])!
-
-        for language in languages{
-            for job in jobs{
-                for career in careers {
-                    for soulFood in soulFoods {
-                        let key = "\(language)\(job)\(career)\(soulFood)"
-                        if db[key] == nil {
-                            db[key] = [score]
-                        } else {
-                            db[key]?.append(score)
-                        }
-                    }
-                }
+func solution(_ dartResult:String) -> Int {
+    let bonus: [Character:Int] = ["S":1, "D":2, "T":3]
+    var result: Int = 0
+    var number: String = ""
+    var data: [Int] = [0,0,0]
+    var index: Int = -1
+    
+    for dart in dartResult{
+        if dart == "*" {
+            if index >= 1 {
+                data[index-1] *= 2
             }
+            data[index] *= 2
+            number = ""
+        } else if dart == "#" {
+            data[index] = -data[index]
+            number = ""
+        } else if dart == "S" || dart == "D" || dart == "T" {
+            data[index] = Int(pow(Double(number)!, Double(bonus[dart]!)))
+            number = ""
+        } else {
+            if number.isEmpty && index < 2 {
+                index += 1
+            }
+            number += String(dart)
         }
     }
-
-    // 이진탐색을 위해 오름차순 정렬
-    let dbCnt = db.count
-    for i in db{
-        let sortArr = i.value.sorted()
-        db[i.key] = sortArr
-    }
-
-    // 마지막 score 비교
-    for i in query{
-        let arr = i.components(separatedBy: " ")
-        let key = "\(arr[0])\(arr[2])\(arr[4])\(arr[6])"
-        let score = Int(arr[7])!
-
-        if let scoreArr = db[key]{
-            // 이진 탐색
-            var low = 0
-            var high = scoreArr.count - 1
-            var mid = 0
-
-            while low <= high {
-                mid = (low + high) / 2
-                if scoreArr[mid] < score {
-                    low = mid + 1
-                } else {
-                    high = mid - 1
-                }
-            }
-            result.append(scoreArr.count - low)
-        } else{
-            result.append(0)
-        }
-    }
+    result = data.reduce(0, +)
     
     return result
 }
 
-print(solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"],["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]))
+print(solution("1D2S#10S"))
