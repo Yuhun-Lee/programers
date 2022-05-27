e01ff604b3a37427124f7ed925834a5542aa6bb4
juhoon <wngns8334@gmail.com>
2022-05-28 05:28:58 +0900

    2016

--- coding test swift/main.swift
+++ coding test swift/main.swift
@@ -1,5 +1,11 @@
-import Foundation
-
-var a: Int = 3
-var result: Double = pow(2.0, a); // 8
-print(result)
+func solution(_ s:String) -> String {
+    
+    if s.count % 2 == 0 {
+        return String(s[s.index(s.startIndex, offsetBy: s.count/2)])
+    }
+    else {
+        return String(s[s.index(s.startIndex, offsetBy: s.count/2 - 1)]) + String(s[s.index(s.startIndex, offsetBy: s.count/2)])
+    }
+    
+    return ""
+}
