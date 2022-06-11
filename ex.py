04b34ba1714dcf0f39e44cff09ef9eb61522d5e4
juhoon <wngns8334@gmail.com>
2022-06-12 06:04:20 +0900

    2206012

--- ex.py
+++ ex.py
@@ -1,12 +1,1 @@
-import math
-
-# 헷갈리는 문법 체크용. (메모장 같은 느낌)
-# a = [1, 2, 3]
-# for i in range(0, len(a)):
-#     a[i] = str(a[i])
-# b = "".join(a)
-# print(b)
-
-exList = ["a", "b", "c", "d", "e", "f", "g"]
-for index, value in enumerate(exList):
-    print(index, value)
+# Python TEST
